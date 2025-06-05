8dd5459f1eb4f0da5788976a38def92bac2d244f
Aiden <93549173+aidenx3vv@users.noreply.github.com>
2025-06-04 23:31:03 -0700

    Update main.cpp to display green circle

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,22 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
+int main() {
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition(sf::Vector2f(650u, 400u));
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
         }
 
         window.clear();
+        window.draw(shape);
         window.display();
     }
 }
